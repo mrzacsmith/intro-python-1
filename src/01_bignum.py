0733c3da62e855dc841b3a81825729faee2d5e17
Zac Smith <zrs3141592@gmail.com>
2020-05-06 19:45:50 -0600

    initial commit

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,8 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+power = 65536
+answer = (2 ** power)
+
+print(f'The answer is {answer}')
+print(f'The total number of digits in 2 ** {power} is {len(str(abs(answer)))}')
